20097e3a7d040091a3659366cd4f29eabe5d1e56
Justin White <justinnwhitee@gmail.com>
2022-08-16 14:49:05 -0400

    commit tree 9389680d96498ef8bf4cc45adc318a94f5415053

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+
+function appendCat(name){
+   return appendCat = [...cats, name];
+} 
+
+function prependCat(name){
+    return prependCat = [name, ...cats];
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
